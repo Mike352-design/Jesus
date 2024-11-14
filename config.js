9bf46b8d0bcbffb08604e42abda169065ac3a492
Mike352-design <patrickamau702@gmail.com>
2024-11-14 21:40:46 +0300

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ module.exports = {
   // ADD Your Session Id 
   MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
     // ADD Your MongoDB Database URL
-  PREFIX: process.env.PREFIX || ".",
+  PREFIX: process.env.PREFIX || "-",
   // Add Your Custom Prefix 
   mode: process.env.mode || "public",
   // Add Your Bot Mode 
@@ -15,7 +15,7 @@ module.exports = {
   // public = AnyOne Working
   // inbox = Only Working  Inbox
   // groups = only working in group
-  OWNER_NUMBER: process.env.OWNER_NUMBER || "263714757857",
+  OWNER_NUMBER: process.env.OWNER_NUMBER || "254104916091",
   //========================================- OTHER - CONFIGS -=====================================================================
   AUTO_VOICE: process.env.AUTO_VOICE || "true",
   ANTI_BAD_WORDS_ENABLED: process.env.ANTI_BAD_WORDS_ENABLED || "true",
@@ -29,8 +29,8 @@ module.exports = {
   ALWAYS_RECORDING: process.env.ALWAYS_RECORDING || "true",
   ANTI_BOT: process.env.ANTI_BOT || "true",
   ANTI_DELETE: process.env.ANTI_DELETE || "true",
-  packname: process.env.packname || "𝙼𝙰𝙻𝚅𝙸𝙽 𝙼𝙳",
-  author: process.env.author || "𝙻𝚘𝚛𝚍 𝙼𝚊𝚕𝚟𝚒𝚗",
+  packname: process.env.packname || "Alvin",
+  author: process.env.author || "𝙻𝚘𝚛𝚍 Al𝚟𝚒𝚗",
   //==========================================- API-CONFIGS -==========================================================
   OPENWEATHER_API_KEY: process.env.OPENWEATHER_API_KEY || "2d61a72574c11c4f36173b627f8cb177", //openweathermap.org
   ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY || "sk_6438bcc100d96458f8de0602aec662f4ba14b905fd090ad3", //elevenlabs.io
@@ -49,16 +49,16 @@ module.exports = {
 //------------------------------------------------------------------------------------------------------------------
 
   
-  START_MSG: process.env.START_MSG || `🚀 *_MALVIN-MD Connected Successfully!_* ✅ 
+  START_MSG: process.env.START_MSG || `🚀 *_ALVIN-MD Connected Successfully!_* ✅ 
 
---- *👨‍💻🎉 _Welcome to MALVIN-MD!_* 🎉🥰 
+--- *👨‍💻🎉 _Welcome to ALVIN-MD!_* 🎉🥰 
 
 *🔹 PREFIX:* .
 
-*🔹 OWNER:* 263714757857
+*🔹 OWNER:* 254104916091
 
 
-_Thank you for using_ *👨‍💻MALVIN MD🥰.*
+_Thank you for using_ *👨‍💻ALVIN MD🥰.*
 _We're here to make your experience enjoyable and seamless._
 _If you need any help or have questions, don't hesitate to ask._ 🌝🥰
 
@@ -74,8 +74,8 @@ _*👨‍💻 Enjoy your time with us! 😊*_
 
 > *©ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴍᴀʟᴠɪɴ ᴋɪɴɢ ᴛᴇᴄʜ*` ,
 
-  ALIVE_IMG: process.env.ALIVE_IMG || "https://g.top4top.io/p_3218pggm51.jpg",
-  MENU_IMG: process.env.MENU_IMG || "https://g.top4top.io/p_3218pggm51.jpg",
+  ALIVE_IMG: process.env.ALIVE_IMG || "https://c.top4top.io/p_32402xbvw0.jpg",
+  MENU_IMG: process.env.MENU_IMG || "https://c.top4top.io/p_32402xbvw0.jpg",
   MENU_MSG: process.env.MENU_MSG || `Menumsg`,
     MENU_MS: process.env.MENU_MS || `menu 2`,
 
